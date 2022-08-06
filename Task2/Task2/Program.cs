dcca19882bb3b6227efee8701c102f97d5976a94
hayk4500 <108433596+hayk4500@users.noreply.github.com>
2022-08-06 04:12:41 +0300

    Домашнее задания для GeekBrains

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.WriteLine("Enter the first number:"); 
+int num1 = Convert.ToInt32(Console.ReadLine()); // Ввод первого числа
+
+Console.WriteLine("Enter the second number:"); 
+int num2 = Convert.ToInt32(Console.ReadLine()); // Ввод второго числа
+
+int max = Math.Max(num1, num2); // Математическая функция нахождения максимума
+int min = Math.Min(num1, num2); // Математическая функция нахождения минимума
+Console.WriteLine("The max number is: " + max); // Вывод результата на экран
+Console.WriteLine("The min number is: " + min); // Вывод результата на экран
---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Enter three numbers: ");
+int num1 = Convert.ToInt32(Console.ReadLine()); // Ввод числа 1
+int num2 = Convert.ToInt32(Console.ReadLine()); // Ввод числа 2
+int num3 = Convert.ToInt32(Console.ReadLine()); // Ввод числа 3
+
+int max = Math.Max(num1, Math.Max(num2, num3)); // Функция определения максимума
+Console.WriteLine("Maximum number is: " + max); // Вывод результата в консоль
