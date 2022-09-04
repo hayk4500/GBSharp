e2d0f2336e6a6d7afad199a8165d16e3d5243de6
hayk4500 <108433596+hayk4500@users.noreply.github.com>
2022-09-04 17:25:56 +0300

    Add 62!

--- Task62/Task62/Program.cs
+++ Task62/Task62/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿Console.Write("Введите размер матрицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+
+int i = 0;
+int j = 0;
+int temp = 1;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+    matrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= matrix.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > matrix.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+WriteArray(matrix);
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+                Console.Write($" {array[i, j]} ");
+
+            else Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
